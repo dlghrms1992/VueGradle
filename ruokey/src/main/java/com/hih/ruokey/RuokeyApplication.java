b7c6cbd4e186134e114022386dc179475e320765
EMG <dlghrms1992@naver.com>
2021-08-21 10:55:42 +0900

    Create Toy Project

---
+++ ruokey/src/main/java/com/hih/ruokey/RuokeyApplication.java
@@ -0,0 +1,13 @@
+package com.hih.ruokey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuokeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuokeyApplication.class, args);
+	}
+
+}
---
+++ ruokey/src/test/java/com/hih/ruokey/RuokeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hih.ruokey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuokeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
