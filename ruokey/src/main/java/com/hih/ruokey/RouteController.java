1b29711d8b4cf397d2511a9383f1d7e5023c58db
HGLEE <HGLEE@DESKTOP-LO6JA8B>
2022-01-27 14:28:15 +0900

    Lombok & log4j 연동

--- ruokey/src/main/java/com/hih/ruokey/RouteController.java
+++ ruokey/src/main/java/com/hih/ruokey/RouteController.java
@@ -1,14 +1,19 @@
 package com.hih.ruokey;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@Slf4j
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class RouteController {
-
+	
 	@GetMapping({"/index","/error"})
 	public String index() {
+		log.debug("index~!!!!!!!!!!!!!!!!!!!!");
 		
 		return "index";
 	}
